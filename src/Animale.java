b88b7c6202b0972bb3c8c7362c7f3b5196ca729f
AleksandarBrkovic <aleksandarbrkovic47@gmail.com>
2022-02-19 11:22:34 +0100

    Prima commit

---
+++ src/Animale.java
@@ -0,0 +1,124 @@
+
+public class Animale {
+	private int velocità;
+	private int forza;
+	private int vita;
+	private int energia;
+	private int attacco;
+	private int difesa;
+	private int danni;
+	private int altezza;
+	private int lunghezza;
+	private int larghezza;
+	private int peso;
+
+	public Animale(int vel, int f, int vit, int ene, int att, int dif, int da, int alt, int lung, int larg, int pe) {
+		vel= velocità;
+		f= forza;
+		vit= vita;
+		ene= energia;
+		att= attacco;
+		dif= difesa;
+		da= danni;
+		alt= altezza;
+		lung= lunghezza;
+		larg= larghezza;
+		pe= peso;
+		
+	}
+
+	public int getVelocità() {
+		return velocità;
+	}
+
+	public void setVelocità(int velocità) {
+		this.velocità = velocità;
+	}
+
+	public int getForza() {
+		return forza;
+	}
+
+	public void setForza(int forza) {
+		this.forza = forza;
+	}
+
+	public int getVita() {
+		return vita;
+	}
+
+	public void setVita(int vita) {
+		this.vita = vita;
+	}
+
+	public int getEnergia() {
+		return energia;
+	}
+
+	public void setEnergia(int energia) {
+		this.energia = energia;
+	}
+
+	public int getAttacco() {
+		return attacco;
+	}
+
+	public void setAttacco(int attacco) {
+		this.attacco = attacco;
+	}
+
+	public int getDifesa() {
+		return difesa;
+	}
+
+	public void setDifesa(int difesa) {
+		this.difesa = difesa;
+	}
+
+	public int getDanni() {
+		return danni;
+	}
+
+	public void setDanni(int danni) {
+		this.danni = danni;
+	}
+
+	public int getAltezza() {
+		return altezza;
+	}
+
+	public void setAltezza(int altezza) {
+		this.altezza = altezza;
+	}
+
+	public int getLunghezza() {
+		return lunghezza;
+	}
+
+	public void setLunghezza(int lunghezza) {
+		this.lunghezza = lunghezza;
+	}
+
+	public int getLarghezza() {
+		return larghezza;
+	}
+
+	public void setLarghezza(int larghezza) {
+		this.larghezza = larghezza;
+	}
+
+	public int getPeso() {
+		return peso;
+	}
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+	
+	public void attacco() {
+		
+	}
+	public void attaccoSpeciale() {
+		
+	}
+}
---
+++ src/Uccelli.java
@@ -0,0 +1,12 @@
+
+public abstract class Uccelli extends Animale {
+
+	public Uccelli(int vel, int f, int vit, int ene, int att, int dif, int da, int alt, int lung, int larg, int pe) {
+		super(vel, f, vit, ene, att, dif, da, alt, lung, larg, pe);
+		
+	}
+	public void attacco(Animale atk ) {
+		
+	}
+	
+}
