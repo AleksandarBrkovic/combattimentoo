4e13c0df971ea4a23ea5e802469786b901b38952
AleksandarBrkovic <aleksandarbrkovic47@gmail.com>
2022-03-02 13:56:42 +0100

    fiume

--- src/Fiume.java
+++ src/Fiume.java
@@ -3,8 +3,10 @@ public class Fiume extends Terreno {
 	public Fiume(String tdt) {
 		super(tdt);
 	}
-public void bonusTerreno() {
-		
+public void bonusTerreno( Animale a) {
+		if(a instanceof Anfibi) {
+										//verifica di che tipo è l'oggetto
+		}
 	
 	}
 }
